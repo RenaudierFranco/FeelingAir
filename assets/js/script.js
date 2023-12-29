015f6c2fc75be36f7dcfeb1e203da20df3140bd2
ignacio soria <soria.ign@gmail.com>
2023-12-29 16:40:28 -0300

    español/ingles

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,26 +7,47 @@ var idiomaActual = 'en';
 function cargarContenidos() {
 
     //JSON con las traducciones
-    fetch(`../../assets/JSON/${idiomaActual}.json`)
+    fetch(`../../assets/JSON/${idiomaActual}.JSON`)
       .then(response => response.json())
       .then(data => {
         // Navbar
-        document.getElementById('HOME').innerText = data.navbar.home;
-        document.getElementById('COMPANY').innerText = data.navbar.company;
-        document.getElementById('FLEET').innerText = data.navbar.fleet;
-        document.getElementById('SERVICES').innerText = data.navbar.services;
-        document.getElementById('CONTACT').innerText = data.navbar.contact;
+        document.getElementById('NAVBAR_HOME').innerText = data.navBar.home;
+        document.getElementById('NAVBAR_COMPANY').innerText = data.navBar.company;
+        document.getElementById('NAVBAR_FLEET').innerText = data.navBar.fleet;
+        document.getElementById('NAVBAR_SERVICES').innerText = data.navBar.services;
+        document.getElementById('NAVBAR_CONTACT').innerText = data.navBar.contact;
         // Quote Form 
-        document.getElementById('ORIGIN').placeholder = data.quoteForm.origin;
-        document.getElementById('DESTINATION').placeholder = data.quoteForm.destination;
-        document.getElementById('DATE').placeholder = data.quoteForm.date;
-        document.getElementById('PAX').placeholder = data.quoteForm.pax;
-        document.getElementById('SEND').innerText = data.quoteForm.send;
+        document.getElementById('QUOTE_FORM_ORIGIN').placeholder = data.quoteForm.origin;
+        document.getElementById('QUOTE_FORM_DESTINATION').placeholder = data.quoteForm.destination;
+        document.getElementById('QUOTE_FORM_DATE').placeholder = data.quoteForm.date;
+        document.getElementById('QUOTE_FORM_PAX').placeholder = data.quoteForm.pax;
+        document.getElementById('QUOTE_FORM_SEND').innerText = data.quoteForm.send;
         // Company
-        document.getElementById('HEADING1').innerText = data.company.heading1;
-        document.getElementById('TEXT1').innerText = data.company.text1;
-        document.getElementById('HEADING2').innerText = data.company.heading2;
-        document.getElementById('TEXT2').innerText = data.company.text2;
+        document.getElementById('COMPANY_HEADING_1').innerText = data.company.heading1;
+        document.getElementById('COMPANY_TEXT_1').innerText = data.company.text1;
+        document.getElementById('COMPANY_HEADING_2').innerText = data.company.heading2;
+        document.getElementById('COMPANY_TEXT_2').innerText = data.company.text2;
+        // Services
+        document.getElementById('SERVICE_SECTION_HEADING').innerText = data.services.heading1;
+        document.getElementById('SERVICE_SECTION_TEXT').innerText = data.services.text1;
+        document.getElementById('SERVICE_SECTION_HEADING_1').innerText = data.services.heading2;
+        document.getElementById('SERVICE_SECTION_TEXT_1').innerText = data.services.text2;
+        document.getElementById('SERVICE_SECTION_HEADING_2').innerText = data.services.heading1;
+        document.getElementById('SERVICE_SECTION_TEXT_2').innerText = data.services.text1;
+        document.getElementById('SERVICE_SECTION_HEADING_3').innerText = data.services.heading3;
+        document.getElementById('SERVICE_SECTION_TEXT_3').innerText = data.services.text3;
+        // Contact Form
+        document.getElementById('CONTACT_HEADING').innerText = data.contact.heading;
+        document.getElementById('CONTACT_FORM_NAME').innerText = data.contact.name;
+        document.getElementById('CONTACT_FORM_EMAIL').innerText = data.contact.email;
+        document.getElementById('CONTACT_FORM_PHONE').innerText = data.contact.phone;
+        document.getElementById('CONTACT_FORM_COMMENTS').placeholder = data.contact.comments;
+        document.getElementById('CONTACT_FORM_SEND').innerText = data.contact.send;
+        // Footer
+        document.getElementById('FOOTER_LINK_1').innerText = data.footer.link1;
+        document.getElementById('FOOTER_LINK_2').innerText = data.footer.link2;
+        document.getElementById('FOOTER_LINK_3').innerText = data.footer.link3;
+        document.getElementById('FOOTER_COPY_RIGHT').innerText = data.footer.copyRight;
       })
       .catch(error => console.error('Error al cargar los contenidos:', error));
   }
@@ -43,6 +64,8 @@ document.getElementById('EN').addEventListener('click', function () {
   
   document.getElementById('ES').addEventListener('click', function () {
     cambiarIdioma('es')
+    console.log('español')
+    console.log('idioma actual', idiomaActual)
     cargarContenidos()
   });
   
